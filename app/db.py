f1d6c671f6f06e34d6abc53e79ccb63682b7658f
saleha1358 <salehamanzari1358@gmail.com>
2026-02-23 12:15:55 +0530

    some code fixes, tested in local and working

--- app/db.py
+++ app/db.py
@@ -1,11 +1,20 @@
 import pymysql
 import os
+import time
 
 def get_db_connection():
-    return pymysql.connect(
-        host=os.environ.get("DB_HOST"),
-        user=os.environ.get("DB_USER"),
-        password=os.environ.get("DB_PASSWORD"),
-        database=os.environ.get("DB_NAME"),
-        cursorclass=pymysql.cursors.Cursor
-    )
+    retries = 5
+    while retries > 0:
+        try:
+            return pymysql.connect(
+                host=os.getenv("DB_HOST"),
+                user=os.getenv("DB_USER"),
+                password=os.getenv("DB_PASSWORD"),
+                database=os.getenv("DB_NAME"),
+                cursorclass=pymysql.cursors.Cursor
+            )
+        except Exception as e:
+            print(f"Database not ready, retrying... {e}")
+            retries -= 1
+            time.sleep(5)
+    raise Exception("Database connection failed after retries")
